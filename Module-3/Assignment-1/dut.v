1f5fe97ede0a44cf600a8e52717a2dd91dd300be
Pavankalyan-k <145740608+Pavankalyan-k@users.noreply.github.com>
2024-07-18 16:34:49 +0530

    Update dut.v

--- Module-3/Assignment-1/dut.v
+++ Module-3/Assignment-1/dut.v
@@ -1,1 +1,10 @@
+module dsp_architecture#(parameter DATA_WIDTH=26,
+DATA_WIDTH_2=16)(
+  input [DATA_WIDTH-1:0] A,
+  input [DATA_WIDTH-1:0] B,
+  input [DATA_WIDTH-1:0] C,
+  output [DATA_WIDTH-1:0] Z
 
+);
+  assign Z=A*B+C;
+endmodule
